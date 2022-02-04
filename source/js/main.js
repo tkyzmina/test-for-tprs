2db51da7f0f2196910519be9ac08c8300822c1e9
tkyzmina <tkyzmina@gmail.com>
2022-02-04 22:09:22 +0300

    fear: add layout for 3 blocks

---
+++ source/js/main.js
@@ -0,0 +1,17 @@
+import { ieFix } from "./utils/ie-fix";
+import { iosVhFix } from "./utils/ios-vh-fix";
+
+import { initModals } from "./modules/init-modals";
+import initAccordion from "./modules/init-accordion";
+
+// Utils
+// ---------------------------------
+
+ieFix();
+iosVhFix();
+
+// Modules
+// ---------------------------------
+
+initModals();
+initAccordion();
---
+++ source/js/modules/init-accordion.js
@@ -0,0 +1,22 @@
+const initAccordion = () => {
+  const accordion = document.querySelectorAll(".accordion");
+  if (accordion) {
+    accordion.forEach(function (item) {
+      const btn = item.children[0];
+      const content = item.children[1];
+
+      btn.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        const maxHeight = content.style.maxHeight;
+        if (maxHeight) {
+          content.style.maxHeight = null;
+          btn.classList.remove("active");
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+          btn.classList.add("active");
+        }
+      });
+    });
+  }
+};
+export default initAccordion;
